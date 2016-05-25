6122178b60814837f03cd784e8a78a7280106935
Adam <AdamBCo@me.com>
2016-05-25 06:43:19 -0700

    Added example links for About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 var About = React.createClass({
   render : function () {
     return (
-      <h3>About Component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application built using React</p>
+        <p>Here are some of the tools I used</p>
+        <ul>
+          <li>
+            <a href='https://facebook.github.io/react'>React</a> - This was the Javascript framework used.
+          </li>
+          <li>
+            <a href='http://openweathermap.org'>OpenWeatherMap</a> - This was the API I used for weather data.
+          </li>
+        </ul>
+      </div>
     );
   }
 });
