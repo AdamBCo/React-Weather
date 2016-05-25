aff8ebe83e977d49dcde44e053d9ddc8c0c7407f
Adam <AdamBCo@me.com>
2016-05-25 06:46:57 -0700

    Updated Get Weather Styles

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>The temperature is {temp} degrees in {location}</h3>
+    <h3 className="text-center">The temperature is {temp} degrees in {location}</h3>
   );
 };
 
