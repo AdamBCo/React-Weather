e67e6f61054b6c35c8f477f335d5c451213b4920
Adam <AdamBCo@me.com>
2016-05-24 22:31:11 -0700

    Updated Navigation Styling

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,15 +1,47 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router')
 
-var Navigation = (props) => {
-  return (
-    <div>
-      <h2>Navigation Component</h2>
-      <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active">About</Link>
-      <Link to="/examples" activeClassName="active">Examples</Link>
-    </div>
-  );
-};
+var Navigation = React.createClass({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('Search Clicked');
+  },
+  render : function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li>
+              <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active">About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active">Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="search" placeholder="Find Stuff" /></li>
+              <li><button type="submit" className="button" value="Get Weather">Search</button></li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 module.exports = Navigation;
+
+
+// <div>
+//   <h2>Navigation Component</h2>
+//   <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+//   <Link to="/about" activeClassName="active">About</Link>
+//   <Link to="/examples" activeClassName="active">Examples</Link>
+// </div>
